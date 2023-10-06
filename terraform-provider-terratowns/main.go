b7b217521225c2d0f75d921c15d2c002257e0d6c
F <filipe.neto616@gmail.com>
2023-10-06 08:26:52 +0000

    #51 wip setting up the skeleton for creating a custom terraform provider

--- terraform-provider-terratowns/main.go
+++ terraform-provider-terratowns/main.go
@@ -7,7 +7,7 @@ package main
 // It shorts for format, which contains 
 // functions for formated I/O.
 import (
-	"log"
+	// "log"
 	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
@@ -18,11 +18,11 @@ import (
 // the program, it starts executing from this function
 func main()  {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: Provider
+		ProviderFunc: Provider,
 	})
 	// Format.PrintLine
 	// prints to standard output
-	// fmt.Println("Hello, world!")
+	fmt.Println("Hello, world!")
 }
 
 // https://developer.hashicorp.com/terraform/tutorials/providers/provider-setup
@@ -31,41 +31,41 @@ func Provider() *schema.Provider{
 	var p *schema.Provider
 	p = &schema.Provider{
 		ResourcesMap: map[string]*schema.Resource{
-			x
+			
 		},
     DataSourcesMap: map[string]*schema.Resource{
-			x
+			
 		},
-		Schema: map[string]*schema.Resource{
+		Schema: map[string]*schema.Schema{
 			"endpoint": {
 				Type: schema.TypeString,
 				Required: true,
-				Description: "The endpoint for the external service"
-			}.
+				Description: "The endpoint for the external service",
+			},
 			"token": {
 				Type: schema.TypeString,
 				Sensitive: true, // make the token as sensitive to hide it in the logs
 				Required: true,
-				Description: "The bearer token for authorization"
+				Description: "The bearer token for authorization",
 			},
 			"user_uuid": {
 				Type: schema.TypeString,
 				Required: true,
 				Description: "The UUID for configuration",
-				ValidateFunc: validateUUID
-			}
-		}
+				// ValidateFunc: validateUUID,
+			},
+		},
 	}
-	p.ConfigureContextFunc = providerConfigure(p)
+	// p.ConfigureContextFunc = providerConfigure(p)
 	return p
 }
 
-func validateUUID(v interface{}, k string) (ws []string, errors []error){
-	log.Print('validateUUID:start')
-	value := v.(string)
-	if _, err = uui.Parse(value); err != nil {
-		errors = append(error, fmt.Errorf("invalid UUID format"))
-	}
-	log.Print('validateUUID:end')
-	return
-}
+// func validateUUID(v interface{}, k string) (ws []string, errors []error){
+// 	log.Print('validateUUID:start')
+// 	value := v.(string)
+// 	if _, err = uui.Parse(value); err != nil {
+// 		errors = append(error, fmt.Errorf("invalid UUID format"))
+// 	}
+// 	log.Print('validateUUID:end')
+// 	return
+// }
