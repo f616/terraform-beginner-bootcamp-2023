b686edbc301ab8731c7dea907a18cbdfe36402fb
F <filipe.neto616@gmail.com>
2023-10-07 15:09:52 +0100

    51 terratowns provider (#57)
    * #51 upload my local go files to be tested in gitpod
    
    * #51 wip setting up the skeleton for creating a custom terraform provider
    
    * #51 wip implement terraform provider block in terraform
    
    * #51 fix typo
    
    * #51 adding build_provider to .gitpod.yml
    
    * #51 go bak to $PROJECT_ROOT at the end of build_provider
    
    * #51  setup skeleton for resources
    
    * #51 finish implementation of terraform provider
    
    * #51 typo correction

--- main.tf
+++ main.tf
@@ -14,9 +14,9 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = "http://localhost:4567/api"
+  user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+  token = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
 
 # module "terrahouse_aws"{
@@ -28,3 +28,15 @@ provider "terratowns" {
 #   content_version = var.content_version
 #   assets_path = var.assets_path
 # }
+
+resource "terratowns_home" "home_arkanoid" {
+  name = "Arkanoid: One of most addictive games in the 80's"
+  description = <<DESCRIPTION
+  Talks about arkanoid. A brief history of the game.
+  The ports and the current evolution of the game.
+  DESCRIPTION
+  # domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "c93msdj.cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
+}
